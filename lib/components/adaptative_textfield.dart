a490c814cf2182ce43ad5399a16b947c9009897a
Lucas <lucaswanderley4@gmail.com>
2020-07-20 21:15:03 -0300

    create adaptative textfield and adaptative datepicker to use ios/android

---
+++ lib/components/adaptative_textfield.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class AdaptativeTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final TextInputType keyboardType;
+  final void Function(String) onSubmitted;
+
+  AdaptativeTextField({
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.onSubmitted,
+    this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: CupertinoTextField(
+              controller: controller,
+              onSubmitted: (_) => onSubmitted,
+              keyboardType: keyboardType,
+              placeholder: label,
+              padding: EdgeInsets.symmetric(
+                horizontal: 6,
+                vertical: 12,
+              ),
+            ),
+          )
+        : TextField(
+            controller: controller,
+            onSubmitted: (_) => onSubmitted,
+            keyboardType: keyboardType,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+          );
+  }
+}
